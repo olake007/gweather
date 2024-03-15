12e168e156cbb97dc931616a357362400d810743
olake007 <palmeoh@pedarecc.sa.edu.au>
2024-03-15 13:56:35 +1030

    added api key and the ability to call the api, added columns of the app

--- app.py
+++ app.py
@@ -11,8 +11,19 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/result')
-def index():
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    api_key = "aa9496e9adb3bf2f85fa97e59f3b278a"
+    form_city = request.form.get('city')
+    form_city = form_city.replace(" ", "")
+    url = ("https://api.openweathermap.org/data/2.5/forecast?q=" + form_city + "&appid=" + api_key)
+
+    print(url)
+
+    response = requests.get(url).json()
+
+
+    print(response)
     return render_template('result.html')
 
 
